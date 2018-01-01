03a3cbd39b3ca564bfcc14bde59ee7092b4308f1
Kuzov <kuzin.o.n@gmail.com>
2018-01-01 19:30:32 +0200

    added editable name

--- app/src/main/java/info/kuzinonline/hellokitty/MainActivity.java
+++ app/src/main/java/info/kuzinonline/hellokitty/MainActivity.java
@@ -3,18 +3,26 @@ package info.kuzinonline.hellokitty;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mHelloTextView;
+    private EditText mNameEditText;
+
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mHelloTextView = (TextView)findViewById(R.id.textView);
+        mHelloTextView = (TextView) findViewById(R.id.textView);
+        mNameEditText = (EditText) findViewById(R.id.editText);
     }
 
     public void onClick(View view) {
-        mHelloTextView.setText("Hello Kitty!");
+        if (mNameEditText.getText().length() == 0) {
+            mHelloTextView.setText("Hello Kitty!");
+        } else {
+            mHelloTextView.setText("Привіт, " + mNameEditText.getText());
+        }
     }
 }
